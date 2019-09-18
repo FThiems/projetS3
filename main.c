ed08ae08693b3067c54d96f2b2cd0e39e8ab094a
FThiems <florian.thiems@gamil.com>
2019-09-18 12:10:15 +0200

    tuto sdl

--- main.c
+++ main.c
@@ -3,9 +3,44 @@
  * \brief Fichier d'execution du projet Ikaruga 2
  */
 
+#include <SDL2/SDL.h>
+
+#include <stdio.h>
+
+
 int main( int argc, char* args[])
 {
-    
+    /* Initialisation simple */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    {
+        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+        return -1;
+    }
+
+    {
+        /* Création de la fenêtre */
+        SDL_Window* pWindow = NULL;
+        pWindow = SDL_CreateWindow("Ikaruga 2",SDL_WINDOWPOS_UNDEFINED,
+                                                                  SDL_WINDOWPOS_UNDEFINED,
+                                                                  960,
+                                                                  720,
+                                                                  SDL_WINDOW_SHOWN);
+
+        if( pWindow )
+        {
+            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+
+            SDL_DestroyWindow(pWindow);
+        }
+        else
+        {
+            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        }
+    }
+
+    SDL_Quit();
+
+    return 0;
     
     
     return 0;
